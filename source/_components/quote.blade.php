45561dbd4db04102e58806f79bdf85233204093c
mattbasile <matthewbasile2@gmail.com>
2022-02-23 08:46:36 -0500

    added mobile responsiveness to quote

--- source/_components/quote.blade.php
+++ source/_components/quote.blade.php
@@ -1,9 +1,9 @@
-<section class="bg-white py-24 px-26 text-center flex flex-col items-center">
-  <img class="h-16 w-16" src="../assets/images/brandmark.svg" alt="Markham Square brandmark">
-  <p class="mt-12 font-medium text-2xl leading-loose max-w-[1000px]">
+<section class="bg-white p-12 md:py-24 md:px-26 text-center flex flex-col items-center">
+  <img class="w-8 h-8 md:h-16 md:w-16" src="../assets/images/brandmark.svg" alt="Markham Square brandmark">
+  <p class="mt-6 md:mt-12 font-medium text-lg md:text-2xl md:leading-loose md:max-w-[1000px]">
     {{$quote}}
   </p>
-  <p class="mt-7 font-cursive text-5xl text-orange-500">
+  <p class="mt-4 md:mt-7 font-cursive text-3xl md:text-5xl text-orange-500">
     {{$name}}
   </p>
   <p class="p3 mt-[8px]">
