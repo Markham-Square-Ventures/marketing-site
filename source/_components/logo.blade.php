f425215b8e56b8c20cf9fec858fdb3bbb551c4dd
Nick Basile <nickbasile@Nicks-MBP-2.attlocal.net>
2022-02-22 23:13:08 -0600

    fixed logo

--- source/_components/logo.blade.php
+++ source/_components/logo.blade.php
@@ -1,5 +1,5 @@
 <img
     src='/assets/images/logo.svg'
     alt="The Markham Square logo of a black square with an orange square on top."
-    class="w-full h-full select-none"
+    class="h-full select-none"
 >
