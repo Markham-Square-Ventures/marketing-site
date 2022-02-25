20eac667f5b78253c28ef460fc67ce7cacd95fd2
mattbasile <matthewbasile2@gmail.com>
2022-02-25 13:08:26 -0500

    seo added

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ $page->language ?? 'en' }}" class="scroll-smooth">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="canonical" href="{{ $page->getUrl() }}">
-        <meta name="description" content="{{ $page->description }}">
-        <title>{{ $page->title }}</title>
+       <x-seo/>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
         <link rel="apple-touch-icon" sizes="180x180" href="../assets/images/favicon/apple-touch-icon.png">
