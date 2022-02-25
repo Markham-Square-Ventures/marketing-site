85a9b044e94042c5de64c71acfb99ef45e11b2d3
mattbasile <matthewbasile2@gmail.com>
2022-02-25 16:00:03 -0500

    adding linkedin

--- source/_components/footer.blade.php
+++ source/_components/footer.blade.php
@@ -20,7 +20,7 @@
       <p class="p3 leading-snug text-white mt-8 lg:mt-0 text-center lg:text-left">&copy; {{ date('Y') }} Markham Square. <br class="sm:hidden"/> All Rights Reserved.</p>
       <div class="flex md:justify-end">
         {{-- LinkedIn --}}
-        <a href="" target="_blank" rel="noopener noreferrer" class="text-white hover:text-orange-500 transition-sm outline-primary">
+        <a href="https://www.linkedin.com/company/markham-square" target="_blank" rel="noopener noreferrer" class="text-white hover:text-orange-500 transition-sm outline-primary">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 lg:h-10 lg:w-10" viewBox="0 0 256 256">
             <rect width="256" height="256" fill="none"></rect>
             <rect x="36" y="36" width="184" height="184" rx="8" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></rect>
@@ -28,16 +28,6 @@
             <line x1="88" y1="112" x2="88" y2="176" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
             <path d="M120,140a28,28,0,0,1,56,0v36" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
             <circle cx="88" cy="80" r="12"></circle>
-          </svg>
-        </a>
-        {{-- Dribbble --}}
-        <a href="" target="_blank" rel="noopener noreferrer" class="ml-8 text-white hover:text-orange-500 transition-sm outline-primary">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 lg:h-10 lg:w-10" viewBox="0 0 256 256">
-            <rect width="256" height="256" fill="none"></rect>
-            <circle cx="128" cy="128" r="96" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></circle>
-            <path d="M71,205.3A160.1,160.1,0,0,1,208,128c5.4,0,10.7.3,16,.8" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
-            <path d="M188,53.1A159.8,159.8,0,0,1,64,112a165.2,165.2,0,0,1-30.1-2.8" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
-            <path d="M85.9,41.7a160.5,160.5,0,0,1,74.3,176.8" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
           </svg>
         </a>
         {{-- Twitter --}}
