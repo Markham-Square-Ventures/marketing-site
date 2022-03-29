2e290dad0ad298c1a098006bc191464a70b6af29
Nick Basile <nicholas.basile@outlook.com>
2022-03-29 15:55:33 -0500

    seo image

--- source/_components/seo.blade.php
+++ source/_components/seo.blade.php
@@ -11,11 +11,11 @@
     <meta property="og:url" content="https://www.markhamsq.com">
     <meta property="og:title" content="{{$title}}">
     <meta property="og:description" content="{{$description}}">
-    <meta property="og:image" content="../assets/images/seo-image.png">
+    <meta property="og:image" content="/assets/images/seo-image.png">
     <meta property="og:image:alt" content="The Markham Square logo on top of interlocking orange chevrons.">
 
     <!-- /* Twitter Large Image Summary Card */ -->
-    <meta name="twitter:card" content="../assets/images/seo-image.png">
+    <meta name="twitter:card" content="/assets/images/seo-image.png">
     <meta name="twitter:site" content="@team_markhamsq">
     <meta name="twitter:creator" content="@team_markhamsq">
     <!-- /* Optional */ -->
